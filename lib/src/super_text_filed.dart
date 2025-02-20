198db52da048a305b48dd1c7ef3467898a986266
ch <473726199@qq.com>
2025-02-20 10:03:12 +0800

    🐛fix: 修复最大行数处理问题

--- lib/src/super_text_filed.dart
+++ lib/src/super_text_filed.dart
@@ -360,7 +360,7 @@ class _SuperTextFiledState extends State<SuperTextFiled> {
       readOnly: widget.readOnly,
       autofocus: widget.autofocus,
       minLines: widget.minLines,
-      maxLines: (widget.minLines != null && widget.maxLength != null && widget.minLines! > widget.maxLines!) ? widget.minLines : widget.maxLines,
+      maxLines: (widget.minLines != null && widget.maxLines != null && widget.minLines! > widget.maxLines!) ? widget.minLines : widget.maxLines,
       obscureText: widget.obscureText,
       textAlign: widget.textAlign,
       enabled: widget.enabled,
